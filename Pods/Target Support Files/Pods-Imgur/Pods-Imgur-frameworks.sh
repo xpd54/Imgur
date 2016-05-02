31a9f8c28f7dcc0e13b3260cfd4abc17ed80f5ea
Ravi Prakash <raviprakash.xpd54@gmail.com>
2016-05-03 01:04:53 +0530

    Add MBProgressHUD library in app

--- Pods/Target Support Files/Pods-Imgur/Pods-Imgur-frameworks.sh
+++ Pods/Target Support Files/Pods-Imgur/Pods-Imgur-frameworks.sh
@@ -85,9 +85,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-Imgur/Alamofire.framework"
+  install_framework "Pods-Imgur/MBProgressHUD.framework"
   install_framework "Pods-Imgur/SDWebImage.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-Imgur/Alamofire.framework"
+  install_framework "Pods-Imgur/MBProgressHUD.framework"
   install_framework "Pods-Imgur/SDWebImage.framework"
 fi
